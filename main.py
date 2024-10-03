2673969c536bfa1d3a7c1cf8c8e18a3a3a309477
Nathalie Boss <100773642+nboss95@users.noreply.github.com>
2024-10-03 18:59:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ def caesar(start_text, shift_amount, cipher_direction):
   if cipher_direction == "decode":
     shift_amount *= -1
   for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    if char in alphabet: # wenn der charakter im Alphabet ist dann:
+
+    if char in alphabet: 
       position = alphabet.index(char)
       new_position = position + shift_amount
       end_text += alphabet[new_position]
@@ -17,14 +14,9 @@ def caesar(start_text, shift_amount, cipher_direction):
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
 
-#TODO-1: Import and print the logo from art.py when the program starts.
 from art import logo
 print(logo)
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
 should_end = False
 while not should_end:
 
